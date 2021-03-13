822218023355cb98027cfb4c7c56154b56aa8309
EndCredits <alicization.han@gmail.com>
2021-03-13 14:36:21 +0800

    treewide: Rebrand for Lewa Revived OS
    *Thanks to Lineage Team & dotOS Team
    *Refs:
    	https://github.com/DotOS/android_build/commit/446d25418fec97dba47211ceaf838ac95cf3ca7a
    	https://github.com/DotOS/android_build/commit/3fe23209a3f128a51028cfc2f03d82edddf5bbb6
    	https://github.com/DotOS/android_build/commit/01e4dc3e5362aba6280ae477da21e72e87c9a53d
    	https://github.com/DotOS/android_build/commit/0ffee8032a1d593c775fc0723e261b35637f1ff0
    	https://github.com/DotOS/android_build/commit/52878f504ba4bb841a418aaff9633fdb05ba5101
    	https://github.com/DotOS/android_build/commit/baeaacc3c342be14d32157d42feb6f903b9fb07d
    	https://github.com/DotOS/android_build/commit/ebc84856e5f53c42e114a50b5ff770c20ba4633a
    	https://github.com/DotOS/android_build/blob/dot11/envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -35,12 +35,6 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - pathmod:    Get the directory containing a module.
 - refreshmod: Refresh list of modules for allmod/gomod.
 
-EOF
-
-    __print_lineage_functions_help
-
-cat <<EOF
-
 Environment options:
 - SANITIZE_HOST: Set to 'address' to use ASAN for all host modules.
 - ANDROID_QUIET_BUILD: set to 'true' to display only the essential messages.
@@ -50,7 +44,7 @@ EOF
     local T=$(gettop)
     local A=""
     local i
-    for i in `cat $T/build/envsetup.sh $T/vendor/lineage/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
+    for i in `cat $T/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       A="$A $i"
     done
     echo $A
@@ -61,8 +55,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/lineage/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
-    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/lineage/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_vars=(`cat $T/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_abs_vars=(`cat $T/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\builtin cd $T; build/soong/soong_ui.bash --dumpvars-mode \
                         --vars="${cached_vars[*]}" \
@@ -144,12 +138,12 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^lineage_") ; then
-        LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
+    if (echo -n $1 | grep -q -e "^lewa_") ; then
+        LEWA_BUILD=$(echo -n $1 | sed -e 's/^lewa_//g')
     else
-        LINEAGE_BUILD=
+        LEWA_BUILD=
     fi
-    export LINEAGE_BUILD
+    export LEWA_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
@@ -592,9 +586,17 @@ function print_lunch_menu()
     local uname=$(uname)
     local choices=$(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES)
     echo
-    echo "You're building on" $uname
-    echo
-    echo "Lunch menu... pick a combo:"
+
+    echo ""
+    tput setaf 1;
+    tput sgr0;
+    echo ""
+    echo "                      Welcome to the device menu                      "
+    echo ""
+    tput bold;
+    echo "     Below are all the devices currently available to be compiled     "
+    tput sgr0;
+    echo ""
 
     local i=1
     local choice
@@ -615,7 +617,10 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo -n "Which would you like? [aosp_arm-eng] "
+        tput setaf 2;
+        tput bold;
+        echo -n "Go ahead and pick a number or enter lunch combo(lewa_device-build_type)... "
+        tput sgr0;
         read answer
     fi
 
@@ -656,24 +661,25 @@ function lunch()
 
     if [ -z "$product" ]
     then
-        echo
-        echo "Invalid lunch combo: $selection"
+        echo ""
+        echo "Come on man, pay attention to what you're doing"
+        echo ""
         return 1
     fi
 
     check_product $product
     if [ $? -ne 0 ]
     then
-        # if we can't find a product, try to grab it off the LineageOS GitHub
+        # if we can't find a product, try to grab it off the Lewa Revived OS Devices GitHub
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lineage/build/tools/roomservice.py $product
+        vendor/lewa/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lineage/build/tools/roomservice.py $product true
+        vendor/lewa/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 
@@ -705,8 +711,6 @@ function lunch()
     export TARGET_BUILD_TYPE=release
 
     echo
-
-    fixup_common_out_dir
 
     set_stuff_for_environment
     printconfig
@@ -1440,6 +1444,18 @@ function _complete_android_module_names() {
     COMPREPLY=( $(allmod | grep -E "^$word") )
 }
 
+# Make using all available CPUs
+function mka() {
+    case `uname -s` in
+        Darwin)
+            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            ;;
+    esac
+}
+
 # Print colored exit condition
 function pez {
     "$@"
@@ -1642,6 +1658,22 @@ validate_current_shell
 source_vendorsetup
 addcompletions
 
-export ANDROID_BUILD_TOP=$(gettop)
+function repopick() {
+    T=$(gettop)
+    $T/vendor/lewa/build/tools/repopick.py $@
+}
 
-. $ANDROID_BUILD_TOP/vendor/lineage/build/envsetup.sh
+# check and set ccache path on envsetup
+if [ -z ${CCACHE_EXEC} ]; then
+    ccache_path=$(which ccache)
+    if [ ! -z "$ccache_path" ]; then
+	export USE_CCACHE=1
+	export CCACHE_COMPRESS=1
+        export CCACHE_EXEC="$ccache_path"
+	echo -e "\e[1mccache enabled and \e[32m\e[4mCCACHE_EXEC\e[0m \e[1mhas been set to : \e[4m$ccache_path\e[0m"
+    else
+        echo -e "\e[31m\e[1mccache not found/installed!\e[0m"
+    fi
+fi
+
+export ANDROID_BUILD_TOP=$(gettop)
